c724733583c4510fd7ee36ec3476fcba12a2b4ed
Hardik Sondagar <hardik.sondagar@blogtard.com>
2014-01-26 14:49:25 +0530

    Version 1 with new Theme

---
+++ assets/js/stripper.js
@@ -0,0 +1,75 @@
+
+$('#convert').on('click',function()
+{
+	
+		//$('#convert i').css({'-webkit-animation':'converting 1s linear','animation':'converting 1s linear','-o-animation':'converting 1s linear','-moz-animation':'converting 1s linear','-ms-animation':'converting 1s linear'});
+                $('#convert i').addClass('fa-spin')
+
+		setTimeout(reset,900);
+		
+		var tags_exclude_list=$(".tag");
+		
+		var tags_exclude_regEx='';
+		$.each( tags_exclude_list, function(index, item){
+				tags_exclude_regEx=(index==0)?$(item).html():tags_exclude_regEx+'|'+$(item).html();
+		});
+		
+		$('#Output').val('');
+
+		//$('#test').css({'display':'block','opacity':'0'});	
+//		$('#test').html($('#Input').val());
+		//var divv = document.createElement('div');
+		//divv.innerHTML=$('#Input').val();
+//		$('#test').find('style').remove();
+//		$('#test').find('iframe').remove();
+//		$('#test').find('style').remove();
+
+		//$('#test').css({'display':'block','opacity':'1'});
+		//$('#test + .shadow').css({'display':'block','margin':'0'});
+		
+
+		var regex_final=/<(?!\s*\/?(a|b|c)\b)[^>]+>/ig;
+		
+		var regex =new RegExp("<(?!\\s*\\/?("+tags_exclude_regEx+")\\b)[^>]+>","gi");
+		console.log(regex);
+		console.log(regex_final);
+//		var regex =(\<)(?!br(\s|\/|\>))(.*?\>);
+//		var body = $('#test').html();
+		 //var regex = /(?!a|br|/a)[^>]+;
+		var body =$('#Input').val();// divv.innerHTML;
+		if(tags_exclude_list.length==0)
+		{
+			regex=/(<([^>]+)>)/ig;
+		}
+		//regex=/(<([^>]+)>)/ig;
+			var result = body.replace(regex, "");
+
+		$('#Output').val(result);
+
+		if($('#Output').val()=='')
+		{
+			$('#Output').css({'display':'none','opacity':'0'});
+			$('#Output').fadeOut("10000");
+                        $('#output-wrap').css({'display':'none','opacity':'0'});
+			$('#output-wrap').fadeOut("10000");
+                        
+
+ 		}
+		else
+		{
+			$('#Output').css({'display':'block','opacity':'1'});
+			$('#Output').fadeIn("10000");
+                        $('#output-wrap').css({'display':'block','opacity':'1'});
+			$('#output-wrap').fadeIn("10000");
+		}
+		
+	
+
+});
+
+function reset()
+{
+		//$('#convert i').css({'-webkit-animation':'none','animation':'none','-o-animation':'none','-moz-animation':'none','-ms-animation':'none'});
+                $('#convert i').removeClass('fa-spin')
+
+}
